322fc07272b125b6085561914075a48a062d3157
Rozhkow <rozhkow.dimas@gmail.com>
2022-12-21 13:42:24 +0300

    feat(firebase): add new function

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const cors = require('cors')({origin: true});
 const admin = require('firebase-admin');
+const fetch = require('node-fetch');
 
 admin.initializeApp();
 
@@ -17,14 +18,40 @@ exports.setUserData = functions.https.onCall((data, context) => {
         return admin
             .firestore()
             .collection('users')
-            .doc(context.auth.uid)
+            .doc(data.uid)
             .update({
+                username: data.username || null,
                 country: data.country || null,
                 city: data.city || null,
-                address: data.address || null
+                postalCode: data.postalCode || null
             }).then(() => {
                 return 'Successfully added';
             })
+    } catch (error) {
+        throw new functions.https.HttpsError(error);
+    }
+});
+
+exports.isUsernameTaken = functions.https.onCall((data, context) => {
+    try {
+        return new Promise((resolve, reject) => {
+            return admin
+                .firestore()
+                .collection('users')
+                .where("username", "==", data.username)
+                .get()
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        if (doc.id) {
+                            resolve(true)
+                        }
+                    });
+                    resolve(false)
+                })
+                .catch((error) => {
+                    reject(error)
+                })
+        })
     } catch (error) {
         throw new functions.https.HttpsError(error);
     }

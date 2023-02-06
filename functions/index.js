8a7d0ea82a0877c61a47835b84662e27e1934edf
Rozhkow <rozhkow.dimas@gmail.com>
2023-02-06 11:54:08 +0300

    feat(emails): add sending emails

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,18 @@
 const functions = require("firebase-functions");
 const cors = require('cors')({origin: true});
 const admin = require('firebase-admin');
-const fetch = require('node-fetch');
+const nodemailer = require('nodemailer');
+
+let mailTransport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    service: 'gmail',
+    auth: {
+        user: 'rozhkow.dimasek@gmail.com',
+        pass: 'wrawwerpkdvqvwje'
+    }
+});
 
 admin.initializeApp();
 
@@ -55,4 +66,32 @@ exports.isUsernameTaken = functions.https.onCall((data, context) => {
     } catch (error) {
         throw new functions.https.HttpsError(error);
     }
+});
+
+exports.sendMail = functions.https.onCall((data, context) => {
+    const recipientEmail = data.recipientEmail || "rozhkow.dimas@gmail.com";
+    console.log('recipientEmail: ' + recipientEmail);
+
+    const mailOptions = {
+        from: 'Abc Support <Abc_Support@gmail.com>',
+        to: recipientEmail,
+        html:
+           `<p style="font-size: 16px;">Thanks for signing up</p>
+            <p style="font-size: 12px;">Stay tuned for more updates soon</p>
+            <p style="font-size: 12px;">Best Regards,</p>
+            <p style="font-size: 12px;">-Support Team</p>
+          ` // email content in HTML
+    };
+
+    mailOptions.subject = 'Welcome to Abc';
+
+    return mailTransport.sendMail(mailOptions).then(() => {
+        console.log('email sent to:', recipientEmail);
+        return new Promise(((resolve, reject) => {
+       
+            return resolve({
+                result: 'email sent to: ' + recipientEmail
+            });
+        }));
+    });
 });

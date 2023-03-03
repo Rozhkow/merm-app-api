6ef6ddd20d1bf61dcfef70578b5bae479e590bf0
Rozhkow <rozhkow.dimas@gmail.com>
2023-03-03 16:34:01 +0300

    fix(mail): fix emil sending function

--- functions/index.js
+++ functions/index.js
@@ -69,21 +69,15 @@ exports.isUsernameTaken = functions.https.onCall((data, context) => {
 });
 
 exports.sendMail = functions.https.onCall((data, context) => {
-    const recipientEmail = data.recipientEmail || "rozhkow.dimas@gmail.com";
-    console.log('recipientEmail: ' + recipientEmail);
+    const { recipientEmail, message, sender } = data;
 
     const mailOptions = {
-        from: 'Abc Support <Abc_Support@gmail.com>',
+        from: 'Mermapp Support <Mermapp>',
         to: recipientEmail,
-        html:
-           `<p style="font-size: 16px;">Thanks for signing up</p>
-            <p style="font-size: 12px;">Stay tuned for more updates soon</p>
-            <p style="font-size: 12px;">Best Regards,</p>
-            <p style="font-size: 12px;">-Support Team</p>
-          ` // email content in HTML
+        html: message ? `<p>${message}</p>` : "<p>Come and join me in MerMapp - it's an app that works as the one-stop-shop for mermaids & mermen all over the world.</p>"
     };
 
-    mailOptions.subject = 'Welcome to Abc';
+    mailOptions.subject = `${sender} invites you to explore MerMapp`;
 
     return mailTransport.sendMail(mailOptions).then(() => {
         console.log('email sent to:', recipientEmail);
